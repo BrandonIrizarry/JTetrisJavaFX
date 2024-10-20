47d4702195f8fed2574671eb21145ae950ae96cb
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-19 22:01:17 -0400

    refactor: remove useless 'throws' header

--- src/main/java/xyz/brandonirizarry/jtetrisjavafx/Main.java
+++ src/main/java/xyz/brandonirizarry/jtetrisjavafx/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) {
         primaryStage.setTitle("JTetris");
 
         var canvas = new Canvas(BOARD_WIDTH, BOARD_HEIGHT);
