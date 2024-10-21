bca394dfd18870a66727f5d9a8bbba7b3599c462
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-20 20:17:23 -0400

    feat: add second canvas for info display

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module xyz.brandonirizarry.JTetrisJavaFX {
     requires xyz.brandonirizarry.JTetrisBackend;
-    requires javafx.graphics;
+    requires javafx.controls;
 
     opens xyz.brandonirizarry.jtetrisjavafx to javafx.fxml;
     exports xyz.brandonirizarry.jtetrisjavafx;
--- src/main/java/xyz/brandonirizarry/jtetrisjavafx/Main.java
+++ src/main/java/xyz/brandonirizarry/jtetrisjavafx/Main.java
@@ -7,6 +7,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.SplitPane;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -23,6 +24,7 @@ public class Main extends Application {
     private static final double SQUARE_UNIT = 25.0;
     private static final double BOARD_HEIGHT = NUM_ROWS * SQUARE_UNIT;
     private static final double BOARD_WIDTH = NUM_COLUMNS * SQUARE_UNIT;
+    private static final double SIDE_WIDTH = BOARD_WIDTH * 0.66;
 
     private static final Game game = new Game(NUM_ROWS, NUM_COLUMNS);
     private static final Queue<KeyCode> keyPresses = new ArrayDeque<>();
@@ -36,13 +38,15 @@ public class Main extends Application {
         var gameCanvas = new Canvas(BOARD_WIDTH, BOARD_HEIGHT);
         gameCanvas.setFocusTraversable(true);
         gameCanvas.setOnKeyPressed(e -> Main.keyPresses.add(e.getCode()));
+        var gameGraphicsContext = gameCanvas.getGraphicsContext2D();
 
-        var graphicsContext = gameCanvas.getGraphicsContext2D();
+        var sideCanvas = new Canvas(SIDE_WIDTH, BOARD_HEIGHT);
+        var sideGraphicsContext = sideCanvas.getGraphicsContext2D();
 
         // This will run the 'update' method 60 times per second
         // "sixty frames per second"
         var mainAnimationLoop = new Timeline(
-                new KeyFrame(Duration.millis(1000.0/30), e -> update(graphicsContext, Main.keyPresses.poll()))
+                new KeyFrame(Duration.millis(1000.0/30), e -> update(gameGraphicsContext, Main.keyPresses.poll()))
         );
 
         var moveDownAnimationLoop = new Timeline(
@@ -54,9 +58,11 @@ public class Main extends Application {
         mainAnimationLoop.play();
         moveDownAnimationLoop.play();
 
-        var pane = new StackPane();
-        pane.getChildren().add(gameCanvas);
-        var scene = new Scene(pane, BOARD_WIDTH, BOARD_HEIGHT);
+        var gamePane = new StackPane(gameCanvas);
+        var sidePane = new StackPane(sideCanvas);
+        var splitPane = new SplitPane(gamePane, sidePane);
+
+        var scene = new Scene(splitPane, BOARD_WIDTH + SIDE_WIDTH, BOARD_HEIGHT);
         primaryStage.setScene(scene);
         primaryStage.setTitle("JTetris");
         primaryStage.show();
