874141799bfe4a02ed8450c9138979cd61e09491
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-26 18:53:51 -0400

    refactor: move DownwardVelocity class to its own file

--- src/main/java/xyz/brandonirizarry/jtetrisjavafx/Main.java
+++ src/main/java/xyz/brandonirizarry/jtetrisjavafx/Main.java
@@ -13,7 +13,6 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import xyz.brandonirizarry.game.DownwardCollisionType;
 import xyz.brandonirizarry.game.Game;
 
 import java.util.ArrayDeque;
@@ -116,48 +115,6 @@ public class Main extends Application {
                     case Garbage -> drawSquare(graphicsContext, rowIndex, columnIndex, Color.DARKGRAY);
                 }
             }
-        }
-    }
-}
-
-class DownwardVelocity {
-    final Timeline animationLoop;
-    final double defaultRate = 1.0;
-    final double fastRate = 20.0;
-    double currentRate = defaultRate;
-
-    DownwardVelocity(Timeline mainAnimationLoop, Game game) {
-        this.animationLoop = new Timeline(
-                new KeyFrame(Duration.millis(1000.0), e -> {
-                    var collisionType = game.moveDown();
-
-                    if (collisionType == DownwardCollisionType.GameLost) {
-                        mainAnimationLoop.pause();
-                    } else if (collisionType != DownwardCollisionType.FreeFall) {
-                        this.decelerate();
-                    }
-                })
-        );
-
-        this.animationLoop.setCycleCount(Animation.INDEFINITE);
-        this.animationLoop.play();
-    }
-
-    private void accelerate() {
-        this.animationLoop.setRate(this.fastRate);
-        this.currentRate = this.fastRate;
-    }
-
-    private void decelerate() {
-        this.animationLoop.setRate(this.defaultRate);
-        this.currentRate = this.defaultRate;
-    }
-
-    void toggleAcceleration() {
-        if (this.currentRate == this.fastRate) {
-            this.decelerate();
-        } else if (this.currentRate == this.defaultRate) {
-            this.accelerate();
         }
     }
 }
