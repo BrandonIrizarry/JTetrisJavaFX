b367de1200442be79ed83360d461eb2b9e34f175
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2024-10-30 11:38:36 -0400

    docs: add Javadoc comment to AnimationDriver

--- src/main/java/xyz/brandonirizarry/jtetrisjavafx/app/AnimationDriver.java
+++ src/main/java/xyz/brandonirizarry/jtetrisjavafx/app/AnimationDriver.java
@@ -5,6 +5,10 @@ import javafx.scene.input.KeyCode;
 import java.util.ArrayDeque;
 import java.util.Queue;
 
+/**
+ * An interface to factor out some commonality between the various animations
+ * our game uses.
+ */
 public interface AnimationDriver {
     Queue<KeyCode> keyPresses = new ArrayDeque<>();
     double frameRate = 30.0;
